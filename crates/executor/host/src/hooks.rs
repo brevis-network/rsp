eb54c667a9ee5322fd9dec36824fa0b9b5b66ab4
Ubuntu <ubuntu@ip-172-31-11-118.us-west-2.compute.internal>
2025-03-17 15:21:20 +0000

    add some probably redundant cpu features and prove_end_log

--- crates/executor/host/src/hooks.rs
+++ crates/executor/host/src/hooks.rs
@@ -2,7 +2,7 @@ use std::{future::Future, time::Duration};
 
 use alloy_consensus::Block;
 use reth_primitives::NodePrimitives;
-use sp1_sdk::{ExecutionReport, SP1VerifyingKey};
+use sp1_sdk::{ExecutionReport, HashableKey, SP1VerifyingKey};
 
 pub trait ExecutionHooks: Send {
     fn on_execution_start(
@@ -26,13 +26,22 @@ pub trait ExecutionHooks: Send {
 
     fn on_proving_end(
         &self,
-        _block_number: u64,
-        _proof_bytes: &[u8],
-        _vk: &SP1VerifyingKey,
-        _execution_report: &ExecutionReport,
-        _proving_duration: Duration,
+        block_number: u64,
+        proof_bytes: &[u8],
+        vk: &SP1VerifyingKey,
+        execution_report: &ExecutionReport,
+        proving_duration: Duration,
     ) -> impl Future<Output = eyre::Result<()>> {
-        async { Ok(()) }
+        async move {
+            println!("on_proving_end triggered!");
+            println!("block_number = {}", block_number);
+            println!("proof_bytes length = {}", proof_bytes.len());
+            println!("verifier_id = {:?}", vk.bytes32());
+            println!("proving_cycles = {:?}", execution_report.total_instruction_count());
+            println!("gas = {:?}", execution_report.gas);
+            println!("proving_duration = {:?}", (proving_duration.as_secs_f32() * 1000.0) as u64);
+            Ok(())
+        }
     }
 }
 
